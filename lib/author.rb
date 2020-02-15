2a63bf28a62056c67f53e85d4197b6f0dbbd9b54
Daniel Sun <danielsun12345@gmail.com>
2020-02-15 01:54:38 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -9,13 +9,13 @@ class Author
     Post.all.select {|posted| posted.author == self}
   end 
 
-  def add_posts(posted)
-    posted.author = self 
+  def add_post(post)
+    post.author = self 
   end 
   
-  def add_posts_by_title(title)
-    posted = Post.new(title)
-    add_posts(posted) 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post) 
   end
   
   def self.post_count
